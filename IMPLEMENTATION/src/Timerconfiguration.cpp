05fce72ae3cade8f54ba5b60d892a8c5f34342f0
MCLEANS <jackagwaya@gmail.com>
2020-03-30 22:34:34 +0300

    Enabling of the specific timer RCC'

--- IMPLEMENTATION/src/Timerconfiguration.cpp
+++ IMPLEMENTATION/src/Timerconfiguration.cpp
@@ -36,6 +36,54 @@ uint16_t Timer_configuration::get_auto_reload_value(void) const{
 	return this->auto_reload_value;
 }
 
+void Timer_configuration::initialize(){
+	//Enable Timer RCC
+	switch(TIMER){
+	case TIM1:
+			RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
+			break;
+	case TIM2:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+			break;
+	case TIM3:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM3EN;
+			break;
+	case TIM4:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;
+			break;
+	case TIM5:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM5EN;
+			break;
+	case TIM6:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM6EN;
+			break;
+	case TIM7:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM7EN;
+			break;
+	case TIM8:
+			RCC->APB2ENR |= RCC_APB2ENR_TIM8EN;
+			break;
+	case TIM9:
+			RCC->APB2ENR |= RCC_APB2ENR_TIM9EN;
+			break;
+	case TIM10:
+			RCC->APB2ENR |= RCC_APB2ENR_TIM10EN;
+			break;
+	case TIM11:
+			RCC->APB2ENR |= RCC_APB2ENR_TIM11EN;
+			break;
+	case TIM12:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM12EN;
+			break;
+	case TIM13:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM13EN;
+			break;
+	case TIM14:
+			RCC->APB1ENR |= RCC_APB1ENR_TIM14EN;
+			break;
+	}
+}
+
 Timer_configuration::~Timer_configuration() {
 	// TODO Auto-generated destructor stub
 }
