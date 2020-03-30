0195b60e0ff77718525e734086187ae162ead276
MCLEANS <jackagwaya@gmail.com>
2020-03-30 22:10:57 +0300

    setting of getter methods as const

--- IMPLEMENTATION/src/Timerconfiguration.h
+++ IMPLEMENTATION/src/Timerconfiguration.h
@@ -15,19 +15,20 @@ namespace custom_libraries {
 class Timer_configuration {
 
 private:
-	uint16_t auto_reload_value;
-	uint16_t prescaler_value;
+	TIM_TypeDef * TIMER; //Timer to configure
+	uint16_t auto_reload_value = 0;
+	uint16_t prescaler_value = 0;
 private:
 
 public:
 
 public:
-	Timer_configuration();
+	Timer_configuration(TIM_TypeDef *TIMER, uint16_t prescaler = 0, uint16_t auto_reload_value = 0);
 	~Timer_configuration();
-	void set_prescaler(uint16_t prescaler_value);
-	uint16_t get_prescaler(void);
-	void set_auto_reload_value(uint16_t auto_reload_value);
-	uint16_t get_auto_reload_value(void);
+	void set_prescaler(uint16_t prescaler_value) ;
+	uint16_t get_prescaler(void) const;
+	void set_auto_reload_value(uint16_t auto_reload_value) ;
+	uint16_t get_auto_reload_value(void) const;
 };
 
 } /* namespace custom_libraries */
