f4130ca56762da9faa5fdb66994ebc08136a0731
MCLEANS <jackagwaya@gmail.com>
2020-03-30 21:55:28 +0300

    Timer configuration source file

---
+++ IMPLEMENTATION/src/Timerconfiguration.cpp
@@ -0,0 +1,21 @@
+/*
+ * Timerconfiguration.cpp
+ *
+ *  Created on: Mar 30, 2020
+ *      Author: MCLEANS
+ */
+
+#include "Timerconfiguration.h"
+
+namespace custom_libraries {
+
+Timer_configuration::Timer_configuration() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Timer_configuration::~Timer_configuration() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace custom_libraries */
