3d8d1551f9f37dc2ebc66b40d14097e81734e6d4
MCLEANS <jackagwaya@gmail.com>
2020-03-30 22:19:29 +0300

    setting the actual auto_reload and prescaler registers

--- IMPLEMENTATION/src/Timerconfiguration.cpp
+++ IMPLEMENTATION/src/Timerconfiguration.cpp
@@ -12,11 +12,14 @@ namespace custom_libraries {
 Timer_configuration::Timer_configuration(TIM_TypeDef *TIMER, uint16_t prescaler_value, uint16_t auto_reload_value) : TIMER(TIMER),
 																												prescaler_value(prescaler_value),
 																												auto_reload_value(auto_reload_value){
-
+		TIMER->PSC = this->prescaler_value;
+		TIMER->ARR = this-> auto_reload_value;
 }
 
 void Timer_configuration::set_prescaler(uint16_t prescaler_value){
 	this->prescaler_value = prescaler_value;
+
+	if(this->prescaler_value != TIMER->PSC) TIMER->PSC = prescaler_value;
 }
 
 uint16_t Timer_configuration::get_prescaler(void) const{
@@ -25,6 +28,8 @@ uint16_t Timer_configuration::get_prescaler(void) const{
 
 void Timer_configuration::set_auto_reload_value(uint16_t auto_reload_value){
 	this->auto_reload_value = auto_reload_value;
+
+	if(this->auto_reload_value != TIMER->ARR) TIMER->ARR = this->auto_reload_value;
 }
 
 uint16_t Timer_configuration::get_auto_reload_value(void) const{
